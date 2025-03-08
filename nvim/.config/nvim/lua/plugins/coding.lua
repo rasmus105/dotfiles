3b2672b1f21a90c6703a82e0e65f35a62d759e36
rasmus105 <rasmus105@protonmail.com>
2025-03-08 20:09:32 +0100

    nvim: added mini.surround

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -61,6 +61,30 @@ return {
 		enabled = true,
 
 		version = "*",
+
 		opts = {},
+	},
+
+	{
+		"echasnovski/mini.surround",
+		enabled = true,
+
+		version = "*",
+
+		opts = {
+			mappings = {
+				add = "ys", -- Add surrounding in Normal and Visual modes
+				delete = "ds", -- Delete surrounding
+				replace = "cs", -- Replace surrounding
+
+				-- Disable the rest
+				update_n_lines = "", -- Update `n_lines`
+				suffix_last = "", -- Suffix to search with "prev" method
+				suffix_next = "", -- Suffix to search with "next" method
+				find = "", -- Find surrounding (to the right)
+				find_left = "", -- Find surrounding (to the left)
+				highlight = "", -- Highlight surrounding
+			},
+		},
 	},
 }
