0907b476bfa0c28dfc7408685159df238fed3caf
Rasmus Kargo <rk@traffitech.com>
2025-10-18 20:03:58 +0200

    feat: fix fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -19,6 +19,14 @@ require('fzf-lua').setup({
     }
 })
 
+local small_window = {
+    fullscreen = false,
+    height = 0.4,
+    width = 0.2,
+    row = 0.5,
+    col = 0.5,
+}
+
 -- Find files
 -- fff is currently lacking some functionality such as full screen and next/prev keybindings
 -- map('n', '\\f', function() require('fff').find_files() end,
@@ -34,6 +42,24 @@ map('n', '\\w', function() require('fzf-lua').live_grep() end,
 map('n', '\\d', function() require('fzf-lua').diagnostics_workspace() end,
     { desc = 'Diagnostics' })
 
+-- Fuzzy find quick fix list
+map('n', '\\q', function() require('fzf-lua').quickfix() end,
+    { desc = 'Quickfix list' })
+
+-- Fuzzy find git diff
+map('n', '\\g', function() require('fzf-lua').git_diff() end,
+    { desc = 'Current git diff' })
+
+-- Fuzzy find buffer git commits
+map('n', '\\b', function() require('fzf-lua').git_bcommits() end,
+    { desc = 'Commit history for buffer' })
+
+-- Fuzzy find colorschemes
+map('n', '\\c', function()
+    require('fzf-lua').colorschemes({
+        winopts = small_window
+    })
+end, { desc = 'Fuzzy find colorschemes' })
 
 -- Grep word on cursor
 map('n', 'gw', function() require('fzf-lua').grep_cword() end,
@@ -47,7 +73,7 @@ map('n', 'gd', function()
     require('fzf-lua').lsp_definitions()
 end, { desc = 'LSP Definitions (fzf-lua)' })
 
--- Make integration (simple, non-persistent)
+-- Make integration
 local standard_makeprg_commands = {
     "make",
     "make clean",
@@ -64,7 +90,7 @@ local standard_makeprg_commands = {
 }
 
 local function select_makeprg_task()
-    local current_makeprg = vim.opt.makeprg:get()
+    local current_makeprg = vim.opt_global.makeprg:get()
     local options = vim.deepcopy(standard_makeprg_commands)
 
     -- Add current makeprg to top if it's not in the standard list and not empty
@@ -82,16 +108,28 @@ local function select_makeprg_task()
     end
 
     require('fzf-lua').fzf_exec(options, {
-        prompt = 'Select makeprg: ',
+        prompt = '> ',
         actions = {
             ['default'] = function(selected)
-                if #selected > 0 then
-                    local makeprg = selected[1]:gsub(" %(current%)", "")
-                    vim.opt.makeprg = makeprg
-                    vim.notify('Set makeprg: ' .. makeprg, vim.log.levels.INFO)
-                end
+                -- Use vim.schedule to defer execution until after fzf closes
+                vim.schedule(function()
+                    vim.notify("selected makeprg!", vim.log.levels.INFO)
+
+                    if #selected > 0 then
+                        local makeprg = selected[1]:gsub(" %(current%)", "")
+
+                        -- Try using vim.cmd instead of vim.opt_global
+                        vim.cmd('set makeprg=' .. vim.fn.escape(makeprg, ' \\|"'))
+
+                        -- Alternative: print to messages
+                        vim.notify('Makeprg set to: ' .. makeprg, vim.log.levels.INFO)
+                    else
+                        vim.notify("No selection made", vim.log.levels.WARN)
+                    end
+                end)
             end
         },
+        winopts = small_window,
     })
 end
 
