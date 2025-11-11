5ac4e30c0ecb0c513a62775a9d98f5f08ed0537e
rasmus105 <rasmus105@protonmail.com>
2025-11-11 11:36:24 +0100

    feat(nvim): remove unwanted colorschemes

--- nvim/.config/nvim/lua/config/vim-pack.lua
+++ nvim/.config/nvim/lua/config/vim-pack.lua
@@ -1,41 +1,35 @@
 vim.pack.add({
-    -- General Utils
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
+	-- General Utils
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 
-    -- File
-    { src = "https://github.com/ThePrimeagen/harpoon",                     version = "harpoon2" },
-    { src = "https://github.com/mikavilpas/yazi.nvim" },
+	-- File
+	{ src = "https://github.com/ThePrimeagen/harpoon", version = "harpoon2" },
+	{ src = "https://github.com/mikavilpas/yazi.nvim" },
 
-    -- Searching & Replacing
-    { src = "https://github.com/ibhagwan/fzf-lua" },
-    -- { src = "https://github.com/dmtrKovalenko/fff.nvim" },
-    { src = "https://github.com/MagicDuck/grug-far.nvim" },
+	-- Searching & Replacing
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	{ src = "https://github.com/MagicDuck/grug-far.nvim" },
 
-    -- Editor improvements
-    { src = "https://github.com/nvim-mini/mini.surround" },
+	-- Editor improvements
+	{ src = "https://github.com/nvim-mini/mini.surround" },
 
-    -- LSP
-    { src = 'https://github.com/neovim/nvim-lspconfig' },
-    { src = 'https://github.com/mason-org/mason.nvim' },
-    { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
-    { src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' },
-    { src = 'https://github.com/Saghen/blink.cmp',                         version = vim.version.range("^1") },
-    { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
-    { src = 'https://github.com/mrcjkb/rustaceanvim' },
+	-- LSP
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
+	{ src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("^1") },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/mrcjkb/rustaceanvim" },
 
-    -- Colorschemes & Visuals
-    { src = "https://github.com/nvim-mini/mini.icons" },
-    { src = "https://github.com/folke/which-key.nvim" },
-    { src = "https://github.com/nvim-lualine/lualine.nvim" },
-    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
-    { src = "https://github.com/nanozuki/tabby.nvim" },
-    { src = "https://github.com/rcarriga/nvim-notify" },
+	-- Colorschemes & Visuals
+	{ src = "https://github.com/nvim-mini/mini.icons" },
+	{ src = "https://github.com/folke/which-key.nvim" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+	{ src = "https://github.com/nanozuki/tabby.nvim" },
+	{ src = "https://github.com/rcarriga/nvim-notify" },
+	{ src = "https://github.com/nvim-mini/mini.cursorword" },
 
-    { src = "https://github.com/catppuccin/nvim" },
-    { src = "https://github.com/vague2k/vague.nvim" },
-    { src = "https://github.com/ellisonleao/gruvbox.nvim" },
-    { src = "https://github.com/rose-pine/neovim" },
-    { src = "https://github.com/rose-pine/neovim" },
-    { src = "https://github.com/rebelot/kanagawa.nvim" },
-    { src = "https://github.com/Yazeed1s/minimal.nvim" },
-});
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
+})
