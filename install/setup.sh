aa0bf8d88924158f9a0973883a27cd6398fb4cb3
rasmus105 <rasmus105@protonmail.com>
2025-11-13 20:28:15 +0100

    feat: use "gum_run" wrapper command to redirect output to log file and show spinner

--- install/setup.sh
+++ install/setup.sh
@@ -6,6 +6,11 @@ DOTFILES_DIR=$(dirname "$SCRIPT_DIR")
 
 source "$DOTFILES_DIR/common.sh"
 
+# Initialize logging
+gum_log_init
+gum_info "Logging to: $INSTALL_LOG_FILE"
+echo
+
 # cache credentials (needed for later installation)
 sudo -v
 
@@ -15,22 +20,19 @@ if ! is_installed "gum"; then
 fi
 
 if ! is_installed "base-devel"; then
-    gum_info "Installing base-devel..."
-    sudo pacman --noconfirm -S base-devel
+    gum_run "Installing base-devel" "sudo pacman --noconfirm -S base-devel"
 fi
+
 if ! is_installed "git"; then
-    gum_info "Installing git..."
-    sudo pacman --noconfirm -S git
+    gum_run "Installing git" "sudo pacman --noconfirm -S git"
 fi
 
 # install paru (AUR helper)
-gum_info "Cloning paru from AUR..."
-rm -rf /tmp/paru
-git clone https://aur.archlinux.org/paru-bin.git /tmp/paru
+gum_info "Setting up paru (AUR helper)..."
+gum_run "Cloning paru from AUR" "rm -rf /tmp/paru && git clone https://aur.archlinux.org/paru-bin.git /tmp/paru"
 cd /tmp/paru
-gum_info "Building and installing paru..."
-makepkg -si --noconfirm
+gum_run "Building and installing paru" "makepkg -si --noconfirm"
 cd ~
-rm -rf /tmp/paru
+gum_run_quiet "rm -rf /tmp/paru"
 gum_success "Paru installed successfully"
 
