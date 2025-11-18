0050e6a01371dd516d5067d416e4853d9f2674c6
rasmus105 <rasmus105@protonmail.com>
2025-11-18 20:12:58 +0100

    fix(nvim): correct keybind description

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -2,4 +2,4 @@ local map = vim.keymap.set
 
 require('render-markdown').setup({})
 
-map('n', '<leader>tm', ':RenderMarkdown toggle<CR>', { desc = "Toggle Markview" })
+map('n', '<leader>tm', ':RenderMarkdown toggle<CR>', { desc = "Toggle Markdown Rendering" })
