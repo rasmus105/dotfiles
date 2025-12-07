ef417f4bc5ae7fb0a6af3e5f8da8a2383f3ef877
rasmus105 <rasmus105@protonmail.com>
2025-12-07 21:50:43 +0100

    fix(font): fix elephant menu for setting font

--- config/elephant/menus/set-system-font.sh
+++ config/elephant/menus/set-system-font.sh
@@ -8,13 +8,15 @@ PROMPT="Select Font "
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "$SCRIPT_DIR/common.sh"
 
-CURRENT_FONT=$(bash "$HOME/.local/bin/system-get-font")
-ALL_FONTS=$(bash "$HOME/.local/bin/system-list-fonts")
+CURRENT_FONT=$(bash -c "$HOME/.local/bin/system-theme font get")
+ALL_FONTS=$(bash -c "$HOME/.local/bin/system-theme font list")
 
 FONT_OPTIONS=$(echo "$ALL_FONTS" | sed "s/^${CURRENT_FONT}$/${SELECTED_PREFIX}${CURRENT_FONT}/")
+# FONT_OPTIONS=${ALL_FONTS//$CURRENT_FONT/$SELECTED_PREFIX$CURRENT_FONT}
 
 SELECTED=$(menu "$PROMPT" "$FONT_OPTIONS")
 
 if [ -n "$SELECTED" ] && ! echo "$SELECTED" | grep -q "$SELECTED_PREFIX"; then
-    bash "$HOME/.local/bin/system-set-font" "$SELECTED"
+    echo "selected: $SELECTED"
+    bash -c "$HOME/.local/bin/system-theme font set '$SELECTED'"
 fi
